a5c6ff218842deb832d9f57a4ae6cd22e5ef4e15
Alexander Gorodilov <gorodilov.alex@gmail.com>
2021-03-10 17:13:00 +0300

    wtf

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,13 +11,26 @@ type App struct {
 	GormController
 }
 
+func (c App) getBooksByAuthor(author string) (book *models.Book) {
+	book = &models.Book{}
+
+	return
+}
+
 func (c App) Index() revel.Result {
 	// book := models.Book{Title: "Jinzhup"} // Note: In practice you should initialize all struct fields
 
 	// if err := c.DB.Create(&book).Error; err != nil {
 	// 	panic(err)
 	// }
-	var book models.Book
-	ans := c.DB.First(&book, "id > 1")
-	return c.RenderJSON(ans)
+	// var book models.Book
+	// ans := c.DB.First(&book, "id > 1")
+	//return c.RenderJSON(ans)
+	return c.Render()
+
+}
+
+func (c App) GetBook() revel.Result {
+
+	return c.Render()
 }
